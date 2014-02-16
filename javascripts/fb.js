822dafc5bc12732f2420e3983bb3d14133d2ecb2
Scott Bale <scott@balehaus.org>
2014-02-15 22:03:24 -0600

    more fb joke

--- javascripts/fb.js
+++ javascripts/fb.js
@@ -1,4 +1,3 @@
-
 window.fbAsyncInit = function() {
     FB.init({
         appId      : "153693303582",
@@ -12,16 +11,16 @@ window.fbAsyncInit = function() {
     if (d.getElementById(id)) return;
     js = d.createElement(s); 
     js.id = id;
-    js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1&appId=153693303582";
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=153693303582";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 function share(s){
     FB.ui({
         method: 'feed',
-        link: 'https://developers.facebook.com/docs/dialogs/',
         app_id: "153693303582",
-        name: 'I got ' + s + '. Which industrial solvent are you?',
+        name: 'Which industrial solvent are you?',
+        caption: 'I got ' + s + '! Which industrial solvent are you?',
         link: 'http://balehaus.org/fb.html',
         picture: 'http://balehaus.org/images/TolueneBromination2006.png'
     }, function(response){});
@@ -29,7 +28,6 @@ function share(s){
 
 $(document).ready(function(){
     $("a.list-group-item").click(function(e) {
-        console.log("click");
         e.preventDefault();
         e.stopPropagation();
         $("div.solvent").hide();
@@ -37,7 +35,7 @@ $(document).ready(function(){
     });
 
     $("div.fb-share-button").click(function(e) {
-        share(e.target.data("solvent"));
+        share($(e.target).data("solvent"));
     });
 });
 
